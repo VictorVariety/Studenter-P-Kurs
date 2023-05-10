4ee245d264d2289379ee432e50fec0fd79380192
VictorVariety <victorv@getacademy.no>
2023-05-10 14:38:51 +0200

    Initial commit

---
+++ Studenter På Kurs/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Klassetrening;
+
+public class Program
+{
+    static bool check = true;
+    static void Main(string[] args)
+    {
+        var students = new List<Studenter>();
+        students.Add(new Studenter("Jonas", 18, "JavaScript", 1));
+        students.Add(new Studenter("Geir", 19, "JavaScript", 1));
+        students.Add(new Studenter("Hans", 28, "C#", 2));
+
+        while (check)
+        {
+            Console.WriteLine("Velg kurs id, for å vise studenter som er på det kurset");
+            var kursId = Convert.ToInt32(Console.ReadLine());
+
+            if (students.Any(student => student.KursId == kursId)) FindStudents(students, kursId);
+            else
+            {
+                Console.WriteLine("Kurset finnes ikke, prøv igjen");
+                continue;
+
+            }
+        }
+    }
+
+    private static void FindStudents(List<Studenter> students, int kursId)
+    {
+        foreach (var student in students.Where(student => student.KursId == kursId))
+        {
+            Console.WriteLine($"Navn: {student.Navn} \nAlder: {student.Alder}");
+            check = false;
+        }
+    }
+}
---
+++ Studenter På Kurs/Studenter.cs
@@ -0,0 +1,17 @@
+﻿namespace Klassetrening;
+
+class Studenter
+{
+    public string? Navn { get; private set; }
+    public int Alder { get; private set; }
+    public string? Kurs { get; private set; }
+    public int KursId { get; private set; }
+
+    public Studenter(string navn, int alder, string kurs, int kursId)
+    {
+        Navn = navn;
+        Alder = alder;
+        Kurs = kurs;
+        KursId = kursId;
+    }
+}
